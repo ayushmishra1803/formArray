832da728d285d971e51846d73b09055b04fabb98
Ayush Mishra <ayushmishra0810@gmail.com>
2021-05-26 03:31:57 +0530

    Chnage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,35 +18,46 @@ export class AppComponent implements OnInit {
     });
   }
   getInitalValueMax() {
-    return (this.Form.get('range') as FormArray).controls[0].get('max').value?(this.Form.get('range') as FormArray).controls[0].get('max').value:'null';
+    return (this.Form.get('range') as FormArray).controls[0].value
+      ? (this.Form.get('range') as FormArray).controls[0].get('max').value
+      : 'null';
   }
   getInitalValueMin() {
-    return (this.Form.get('range') as FormArray).controls[0].get('max').value?(this.Form.get('range') as FormArray).controls[0].get('min').value:'null';
+    return (this.Form.get('range') as FormArray).controls[0].value
+      ? (this.Form.get('range') as FormArray).controls[0].get('min').value
+      : 'null';
   }
-  getInitalValueMaxTrue(){
-    return (this.Form.get('range') as FormArray).controls[0].get('max').value?(this.Form.get('range') as FormArray).controls[0].value:'null'
+  getInitalValueMaxTrue() {
+    return (this.Form.get('range') as FormArray).controls[0].value
+      ? (this.Form.get('range') as FormArray).controls[0].value
+      : 'null';
   }
   chnageInitalValue(value) {
-    (this.Form.get('range') as FormArray).controls.splice(0,1)
+    (this.Form.get('range') as FormArray).controls.splice(0, 1);
     if (value === 'min') {
-      (this.Form.get('range') as FormArray).controls.push(new FormGroup({
-        min:new FormControl(100)
-      }))
+      (this.Form.get('range') as FormArray).controls.push(
+        new FormGroup({
+          min: new FormControl(100),
+        })
+      );
       console.log((this.Form.get('range') as FormArray).controls[0].value);
-
     }
     if (value === 'max') {
-      (this.Form.get('range') as FormArray).controls.push(new FormGroup({
-        max:new FormControl(3000)
-      }))
+      (this.Form.get('range') as FormArray).controls.push(
+        new FormGroup({
+          max: new FormControl(3000),
+        })
+      );
       console.log((this.Form.get('range') as FormArray).controls[0].value);
     }
     if (value === 'true') {
-      (this.Form.get('range') as FormArray).controls.push(new FormGroup({
-        min:new FormControl(100),
-        max:new FormControl(3000)
-      }))
-      console.log((this.Form.get('range') as FormArray).controls[0].value)
+      (this.Form.get('range') as FormArray).controls.push(
+        new FormGroup({
+          min: new FormControl(100),
+          max: new FormControl(3000),
+        })
+      );
+      console.log((this.Form.get('range') as FormArray).controls[0].value);
     }
   }
 }
